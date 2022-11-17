cff514f29fbdaeb99878dbec10a851da27055ff7
igorjba <igorjbahia@gmail.com>
2022-11-17 20:35:18 -0300

    :sparkles: feat: Exercício 02 concluído.

---
+++ 02/index.js
@@ -0,0 +1,27 @@
+const carro1 = {
+    marca: "Volkswagen",
+    modelo: "Chevette",
+    ano: 2000,
+    cor: "vermelho",
+    quantidade_portas: 4,
+    automatico: false
+};
+
+const carro2 = {
+    marca: "Volkswagen",
+    modelo: "Fusca",
+    ano: 1980,
+    cor: "branco",
+    quantidade_portas: 4,
+    automatico: true
+};
+
+const carro3 = {
+    marca: "Fiat",
+    modelo: "Uno",
+    ano: 2008,
+    cor: "prata",
+    quantidade_portas: 2,
+    automatico: false
+};
+
