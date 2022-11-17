20944a9b352ace18179449cfc9871eac3529e1df
igorjba <igorjbahia@gmail.com>
2022-11-17 19:18:32 -0300

    First commit

---
+++ 01/index.js
@@ -0,0 +1,10 @@
+const endereço = {
+    rua: "Ladeira do Sossego",
+    numero: 171,
+    complemento: "Sei não",
+    bairo: "Lá ele",
+    cep: 40000000,
+    cidade: "Salvador",
+    estado: "Bahia",
+    pais: "Brasil"
+}
