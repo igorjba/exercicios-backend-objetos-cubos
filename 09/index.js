f542d1f4bb550ccf8a23ca33a418ed2570936315
igorjba <igorjbahia@gmail.com>
2022-11-18 09:07:12 -0300

    :sparkles: feat: Exercício 09 resolvido.

---
+++ 09/index.js
@@ -0,0 +1,28 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+];
+
+const participante = "Carlos";
+
+for (let i = 0; i < participantes.length; i++) {
+    if (participantes[i].nome === participante) {
+        console.log(`Galera... O ${participantes[i].nome} está na posição ${i + 1}, corre lá!`)
+    }
+}
