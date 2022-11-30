b63c3355ae033e945dcd68ae5f58a367bfaf27c7
igorjba <igorjbahia@gmail.com>
2022-11-30 10:22:57 -0300

    :recycle: refactor: Ex02 Todos os objetos foram inseridos em um único array.

--- 02/index.js
+++ 02/index.js
@@ -1,27 +1,25 @@
-const carro1 = {
+const carros = [{
     marca: "Volkswagen",
     modelo: "Chevette",
     ano: 2000,
     cor: "vermelho",
     quantidade_portas: 4,
     automatico: false
-};
-
-const carro2 = {
+}, {
     marca: "Volkswagen",
     modelo: "Fusca",
     ano: 1980,
     cor: "branco",
     quantidade_portas: 4,
     automatico: true
-};
-
-const carro3 = {
+}, {
     marca: "Fiat",
     modelo: "Uno",
     ano: 2008,
     cor: "prata",
     quantidade_portas: 2,
     automatico: false
-};
+}
+];
 
+console.log(carros);
